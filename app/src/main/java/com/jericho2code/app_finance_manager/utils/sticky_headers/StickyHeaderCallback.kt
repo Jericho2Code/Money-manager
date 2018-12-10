f1e49f65781fc53b96fc1a82e56fbd5e826d14e3
Mikhail Panchukov <jericho2.0@yandex.ru>
2018-12-10 19:39:24 +0300

    [core]
    * delete unused files

--- app/src/main/java/com/jericho2code/app_finance_manager/utils/sticky_headers/StickyHeaderCallback.kt
+++
@@ -1,9 +0,0 @@
-package ru.kinoplan24.app.presentation.utils.sticky_headers
-
-import android.view.View
-
-interface StickyHeaderCallback<T> {
-    fun hasHeader(position: Int): Boolean
-    fun header(position: Int): T?
-    fun bindHeaderView(parentView: View, headerValue: T)
-}
